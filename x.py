db34f18c255ba082ea193113a08d0258540f2317
Rddsdxfd <154103141+Rddsdxfd@users.noreply.github.com>
2023-12-20 13:26:13 +0200

    Update x.py

--- x.py
+++ x.py
@@ -4,60 +4,57 @@ import cv2
 import numpy as np
 import pytesseract
 import re
+import tempfile
+import os
 
 bot = telebot.TeleBot("6503263167:AAFLTBgytJpQ4FegGlUI2qGaHaHrXFL9rs8")
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, "Hello! I am a bot that can read text from videos. Send me a video and I will highlight the text and send it to you.")
+# ... (other parts of your code)
 
 @bot.message_handler(content_types=['video'])
 def handle_video(message):
     try:
         file_info = bot.get_file(message.video.file_id)
-        downloaded_file = bot.download_file(file_info.file_path)
-
-        if file_info.file_size > 10 * 1024 * 1024:
+        
+        if message.video.file_size > 10 * 1024 * 1024:
             bot.send_message(message.chat.id, "The video is too large. Please send a video that is less than 10 MB.")
             return
-
+        
+        downloaded_file = bot.download_file(file_info.file_path)
         nparr = np.frombuffer(downloaded_file, np.uint8)
+        with tempfile.NamedTemporaryFile(delete=False, suffix='.mp4') as temp_file:
+            temp_file.write(nparr)
+            temp_filename = temp_file.name
 
-        cap = cv2.VideoCapture(cv2.imdecode(nparr, cv2.IMREAD_COLOR))
-
+        cap = cv2.VideoCapture(temp_filename)
         extracted_text = []
 
         while cap.isOpened():
             ret, frame = cap.read()
-
             if not ret:
                 break
 
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            # Preprocess the frame here, if necessary
 
-            thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+            # Consider adding frame resizing if the frames are large
+            # to speed up processing and decrease memory usage
 
-            labels = cv2.connectedComponentsWithStats(thresh, connectivity=4)[1]
-
-            for label in np.unique(labels)[1:]:
-                x, y, w, h = cv2.boundingRect(labels == label)
-
-                cropped_thresh = thresh[y:y+h, x:x+w]
-
-                text = pytesseract.image_to_string(cropped_thresh, lang='rus')
-
-                if text:
-                    extracted_text.append(text)
+            text = pytesseract.image_to_string(frame, lang='rus')  # Change to the correct language if not 'rus'
+            if text.strip():  # Checks if text is non-empty and not just whitespace
+                extracted_text.append(text.strip())
 
         cap.release()
+        os.unlink(temp_filename)  # Delete the temporary file
 
         if not extracted_text:
             bot.send_message(message.chat.id, "No text was found in the video.")
         else:
             bot.send_message(message.chat.id, '\n'.join(extracted_text))
-
     except Exception as e:
         error_message = f"An error occurred while processing the video. Error details: {str(e)}"
         bot.send_message(message.chat.id, error_message)
 
-bot.polling()
+# ... (rest of your code)
+
+# Start bot polling
+bot.polling(non_stop=True, interval=0)
