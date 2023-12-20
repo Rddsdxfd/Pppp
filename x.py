9587c0d4e9dc8d36523f76681ae3b62c97b7e6ba
Rddsdxfd <154103141+Rddsdxfd@users.noreply.github.com>
2023-12-20 16:25:10 +0200

    Update x.py

--- x.py
+++ x.py
@@ -15,11 +15,11 @@ bot = telebot.TeleBot("6503263167:AAFLTBgytJpQ4FegGlUI2qGaHaHrXFL9rs8")
 def handle_video(message):
     try:
         file_info = bot.get_file(message.video.file_id)
-        
+
         if message.video.file_size > 10 * 1024 * 1024:
             bot.send_message(message.chat.id, "The video is too large. Please send a video that is less than 10 MB.")
             return
-        
+
         downloaded_file = bot.download_file(file_info.file_path)
         nparr = np.frombuffer(downloaded_file, np.uint8)
         with tempfile.NamedTemporaryFile(delete=False, suffix='.mp4') as temp_file:
@@ -29,22 +29,35 @@ def handle_video(message):
         cap = cv2.VideoCapture(temp_filename)
         extracted_text = []
         frame_count = 0
+        seen_word_combinations = set()
 
         while cap.isOpened():
             ret, frame = cap.read()
             if not ret:
                 break
 
-            # Process every 25th frame
-            if frame_count % 25 == 0:
-                # Preprocess the frame here, if necessary
+            # Process every 15th frame
+            if frame_count % 15 == 0:
+                # Preprocess the frame
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+                edges = cv2.Canny(thresh, 100, 200)
 
                 # Consider adding frame resizing if the frames are large
                 # to speed up processing and decrease memory usage
 
-                text = pytesseract.image_to_string(frame, lang='rus')  # Change to the correct language if not 'rus'
+                text = pytesseract.image_to_string(edges, lang='rus')  # Change to the correct language if not 'rus'
                 if text.strip():  # Checks if text is non-empty and not just whitespace
-                    extracted_text.append(text.strip())
+                    # Split the text into words
+                    words = text.split()
+
+                    # Create a word combination by joining the first two words
+                    word_combination = ' '.join(words[:2])
+
+                    # Check if the word combination has been seen before
+                    if word_combination not in seen_word_combinations:
+                        seen_word_combinations.add(word_combination)
+                        extracted_text.append(text.strip())
 
             frame_count += 1
 
@@ -54,7 +67,12 @@ def handle_video(message):
         if not extracted_text:
             bot.send_message(message.chat.id, "No text was found in the video.")
         else:
-            bot.send_message(message.chat.id, '\n'.join(extracted_text))
+            # Post-process the extracted text
+            extracted_text = ' '.join(extracted_text)  # Join all the text fragments into a single string
+            extracted_text = re.sub(r'[^а-яА-ЯёЁa-zA-Z0-9\s]', '', extracted_text)  # Remove non-alphanumeric characters
+            extracted_text = re.sub(r'\s+', ' ', extracted_text)  # Remove extra whitespace
+
+            bot.send_message(message.chat.id, extracted_text)
     except Exception as e:
         error_message = f"An error occurred while processing the video. Error details: {str(e)}"
         bot.send_message(message.chat.id, error_message)
