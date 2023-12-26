089e4fae812cade7e5f12034d12f539a3d8ceec3
Rddsdxfd <154103141+Rddsdxfd@users.noreply.github.com>
2023-12-26 22:11:18 +0200

    Update X2.py

--- X2.py
+++ X2.py
@@ -1,50 +1,78 @@
-import telebot
-from telebot import types
-from moviepy.editor import VideoFileClip
-import speech_recognition as sr
+import telebot 
+import requests
 import tempfile
+from moviepy.editor import VideoFileClip
 import os
+
+API_KEY = "e2202adb69a843dc84e20792f7b2f7e2" 
 
 bot = telebot.TeleBot("6503263167:AAFLTBgytJpQ4FegGlUI2qGaHaHrXFL9rs8")
 
-@bot.message_handler(content_types=['video'])
+@bot.message_handler(content_types=['video']) 
 def handle_video(message):
+
     try:
         file_info = bot.get_file(message.video.file_id)
-        
         downloaded_file = bot.download_file(file_info.file_path)
-        with tempfile.NamedTemporaryFile(delete=False, suffix='.mp4') as temp_video_file, \
-             tempfile.NamedTemporaryFile(delete=False, suffix='.wav') as temp_audio_file:
-            temp_video_file.write(downloaded_file)
-            temp_video_filename = temp_video_file.name
-            temp_audio_filename = temp_audio_file.name
-
-        # Extract audio from video
-        video_clip = VideoFileClip(temp_video_filename)
-        audio_clip = video_clip.audio
-        audio_clip.write_audiofile(temp_audio_filename)
-
-        # Recognize audio
-        recognizer = sr.Recognizer()
-        with sr.AudioFile(temp_audio_filename) as source:
-            audio_data = recognizer.record(source)
-            text = recognizer.recognize_google(audio_data)
         
-        # Split text into chunks to avoid long message errors
-        text_chunks = [text[i:i+3500] for i in range(0, len(text), 3500)]
+        with tempfile.NamedTemporaryFile(suffix='.mp4') as video_file:
+            video_file.write(downloaded_file)
+    
+            clip = VideoFileClip(video_file.name)
+            audio = clip.audio
+            audio.write_audiofile("audio.wav")
+
+        upload_response = upload_audio("audio.wav")
+        transcript_id = upload_response["id"]
+
+        poller = Poller(API_KEY, transcript_id)
+        transcript = poller.poll_for_transcription()
         
+        text = transcript['text']
+        text_chunks = chunk_text(text) 
+
+        first_msg = None
         for chunk in text_chunks:
-            bot.reply_to(message, chunk)
-
-    except Exception as e:
-        bot.reply_to(message, f"An error occurred: {str(e)}")
-
+            if not first_msg:
+               first_msg = bot.send_message(message.chat.id, chunk) 
+            else:
+               bot.send_message(message.chat.id, chunk, reply_to_message_id=first_msg.message_id)
+       
     finally:
-        # Cleanup temporary files
-        for temp_file in [temp_video_filename, temp_audio_filename]:
-            if os.path.exists(temp_file):
-                os.unlink(temp_file)
-                
-# Rest of bot code...
+        os.remove("audio.wav")
+        
+# Helper functions        
+def upload_audio(filename):
+   with open(filename, 'rb') as fin:
+      file_data = fin.read()
 
+   url = "https://api.assemblyai.com/v2/upload"
+   headers = {'authorization': API_KEY} 
+   response = requests.post(url, headers=headers, data=file_data)
+   return response.json()
+
+def chunk_text(text, max_chars=3500):
+   return [text[i:i+max_chars] for i in range(0, len(text), max_chars)] 
+   
+# AssemblyAI poller class   
+class Poller:
+    def __init__(self, api_key, transcript_id):
+        self.api_key = api_key
+        self.transcript_id = transcript_id
+        self.polling_interval = 30
+        self.status = ""
+
+    def poll_for_transcription(self):
+       url = f"https://api.assemblyai.com/v2/transcript/{self.transcript_id}"
+       headers = {'authorization': self.api_key}
+       response = requests.get(url, headers=headers).json()
+       self.status = response['status']
+
+       while self.status != 'completed':
+          sleep(self.polling_interval)
+          response = requests.get(url, headers=headers).json() 
+          self.status = response['status']
+
+       return response
+       
 bot.polling()
